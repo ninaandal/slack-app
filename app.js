69522dcf74f47dfd578170716cea021095c9eb11
Nina Andal Aarvik <nina@sanity.io>
2024-04-15 17:56:41 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require("dotenv").config({ path: [".env.local", ".env"] });
+
+const { App } = require("@slack/bolt");
+const { FileInstallationStore } = require("@slack/oauth");
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  installationStore: new FileInstallationStore(),
+});
+
+console.log(process.env);
+
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
+
+app.action("button_click", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+// app.event("app_home_opened", ({ event, say }) => {
+//   say(`Hey user, <@${event.user}>!`);
+// });
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
